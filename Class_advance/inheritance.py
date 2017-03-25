3e4063d57793b32955b0a0c14174051983efc8c0
정지원 <4dakto@gmail.com>
2017-03-26 00:57:27 +0900

    Jiwon ::  added Inheritance py

---
+++ Class_advance/inheritance.py
@@ -0,0 +1,25 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constuctor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+    
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+    
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Eye Color - " + str(self.number_of_toys))
+
+billy_cyrus = Parent("Cyrus", "blue")
+# billy_cyrus.show_info()
+mailey_cyrus = Child("Cyrus", "Blue", 5)
+mailey_cyrus.show_info()
